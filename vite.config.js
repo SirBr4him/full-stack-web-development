7c86bf3c889d36e8273c7e68c0ded7c4705c21fe
Brahim Benlarbi <30289709+SirBr4him@users.noreply.github.com>
2022-07-15 20:39:45 +0000

    configured vite server to work with external urls

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: process.env.HRM_HOST ? 443 : 5173,
+		host: process.env.HRM_HOST
+			? process.env.HRM_HOST.substring('https://'.length)
+			: 'localhost'
+	}
 };
 
 export default config;
